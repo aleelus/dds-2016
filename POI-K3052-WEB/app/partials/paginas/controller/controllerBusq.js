c421a1e15ea6adfd0eff0ff689bc2707c19bc53d
Alejandro Elustondo <aleelus@hotmail.com>
2016-09-07 15:38:23 -0300

    ahora los resultados no se repiten y agrego una validacion cuando agrego un criterio

--- POI-K3052-WEB/app/partials/paginas/controller/controllerBusq.js
+++ POI-K3052-WEB/app/partials/paginas/controller/controllerBusq.js
@@ -3,17 +3,37 @@ function busquedaController(busqueda) {
 
     self.busquedas = busqueda;
     self.criterios = criterios;
+    self.listaResultados = listaResultados;
 
-    self.buscarPorCriterio = function (criterio) {
+    self.buscarPorCriterio = function () {
 
         var i = 0;
 
-        var esta = function (vec) {
+
+
+        var esta = function (vec,criterio) {
             return _.includes(vec, criterio);
         };
+        /*
         return _.filter(self.busquedas, function (i) {
             return (i.nombre.includes(criterio) || i.direccion.includes(criterio) || esta(i.tipo.rubros) || esta(i.servicios) || esta(i.tipo.linea));
-        })
+        })*/
+
+        //HAY Q VER BIEN DESPUES ESTA FUNCION
+        var x;
+
+        var filtrado = [];
+
+        self.listaResultados=[];
+        for(x=0;x<self.criterios.length;x++){
+            filtrado = _.filter(self.busquedas, function (i) {
+                return (i.nombre.includes(self.criterios[x].nombre) || i.direccion.includes(self.criterios[x].nombre) || esta(i.tipo.rubros,self.criterios[x].nombre) || esta(i.tipo.servicios,self.criterios[x].nombre) || esta(i.tipo.linea,self.criterios[x].nombre));
+            })
+            self.listaResultados=_.union(filtrado,self.listaResultados);
+
+        }
+
+        return self.listaResultados;
     };
 
 }
@@ -22,8 +42,9 @@ function criterioController(criterio) {
     self.criterios = criterio;
     self.nuevoCriterio = '';
 
+
     self.agregarCriterio = function () {
-        if (_.find(self.criterios, {nombre: self.nuevoCriterio}) === undefined) {
+        if (_.find(self.criterios, {nombre: self.nuevoCriterio}) === undefined && self.nuevoCriterio!== '') {
             self.criterios.push(new Criterio(self.nuevoCriterio));
         }
         self.nuevoCriterio = '';
@@ -36,6 +57,8 @@ function criterioController(criterio) {
         _.remove(self.criterios, function (actual) {
             return (actual.nombre === nombre);
         })
+
+
     };
 
     self.setValue = function (criterio) {
